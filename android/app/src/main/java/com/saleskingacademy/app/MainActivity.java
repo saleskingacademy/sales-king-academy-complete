b449226e4d5dc38cf5d9970b64c0c03847cbcf97
saleskingacademy <saleskingacademy@hotmail.com>
2025-12-11 13:21:49 -0600

    Add android/app/src/main/java/com/saleskingacademy/app/MainActivity.java

---
+++ android/app/src/main/java/com/saleskingacademy/app/MainActivity.java
@@ -0,0 +1,39 @@
+package com.saleskingacademy.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        
+        webView.loadUrl("https://saleskingacademy.com");
+        
+        setContentView(webView);
+    }
+    
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
